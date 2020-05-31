642ceb9eea3fe9721a3b1ff24cda81f11697c691
Elin Gustafsson <elin.b.gustafsson@gmail.com>
2020-05-31 20:58:22 +0200

    update commit

--- wp-content/plugins/vue-frontend/vuecode.js
+++
@@ -1,6 +0,0 @@
-var app = new Vue({
-  el: '#divWpVue',
-  data: {
-    iframe: "https://www.yumpu.com/sv/embed/view/7LQSEdhnMfjkbBrN"
-  }
-})
--- wp-content/plugins/vue-readbook/vuecode-readbook.js
+++
@@ -1,8 +0,0 @@
-var app = new Vue({
-    el: '#iframe',
-    data: {
-      message: 'Hello Vue!',
-      text: 'Här är en bra',
-      image: '<iframe width="940px" height="610px" src="https://www.yumpu.com/sv/embed/view/acWFh7zDn3QiTyH2" frameborder="0" allowfullscreen="true"  allowtransparency="true"></iframe>'
-    }
-  })
